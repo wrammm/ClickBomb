ad37bb498faa26234b73531653e88dd0a67ed0a9
Billy <42595835+wrammm@users.noreply.github.com>
2022-12-17 15:35:18 -0600

    Added click bomb app

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  firstClick = false;
+  clickThreshold = 30000;
+  finished = false;
+  code = 12345678;
+
+  ngOnInit() {
+    const clicksRemaining = localStorage.getItem('clicksRemaining');
+    if(clicksRemaining && clicksRemaining !== null) {
+      this.clickThreshold = Number(clicksRemaining);
+      this.firstClick = true;
+      if(this.clickThreshold <= 0) {
+        this.finished = true;
+      }
+    }
+  }
+
+  clickedScreen() {
+    this.clickThreshold --;
+    this.firstClick = true;
+    if(this.clickThreshold <= 0) {
+      this.finished = true;
+    }
+    localStorage.setItem('clicksRemaining', this.clickThreshold.toString());
+  }
+
+}
